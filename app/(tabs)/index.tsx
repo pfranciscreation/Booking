6a02ebe62db21298fb61b9f705dd759094838a4e
pfranciscreation <pfrancisbusiness@gmail.com>
2025-09-02 14:51:29 -0400

    Test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,8 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome</ThemedText>
+        <ThemedText type="title">Hello World!</ThemedText>
         <ThemedText type="title">Hello World!</ThemedText>
         <HelloWave />
       </ThemedView>
